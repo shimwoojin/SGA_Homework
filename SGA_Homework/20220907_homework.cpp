1f42be0a9f93d35de93e2abcb82d2f48fe04438a
Shimwoojin <70691442+shimwoojin@users.noreply.github.com>
2022-09-15 18:40:48 +0900

    20220907_homework

---
+++ SGA_Homework/20220907_homework.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("안녕하세요. 제 소개를 간단히 할게요.\n");
+	printf("\t이름 : 심우진\n");
+	printf("\t나이 : 26\n");
+	printf("\t혈액형 : \"A\"형\n");
+	printf("감사합니다. 잘 지내봅시다.\n");
+
+	return 0; 
+}
